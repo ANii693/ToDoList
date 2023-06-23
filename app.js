e1ece68d075565e2bffae81a6ec192fc4af840b1
ANIRUDH RAI <anirudhrai503@gmail.com>
2023-06-23 20:53:19 +0530

    commit all

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+const items = [];
+const wrokItem = [];
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  let day = date.getDate();
+  res.render("list", { listTitle: day, newAddedItems: items });
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  if (req.body.list === "Work") {
+    wrokItem.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newAddedItems: wrokItem });
+});
+
+// app.post("/work",function(req,res){
+//   let item = req.body.newItem;
+//   wrokItem.push(item);
+//   res.redirect("/work");
+// });
+
+
+app.listen(3000, function () {
+  console.log("server is started on port 3000");
+});
